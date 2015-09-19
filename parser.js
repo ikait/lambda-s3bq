2d0a30cea193b51d5cf167b8efa800741d5722c0
Taishi Ikai <t@ikai.tokyo>
2015-09-20 02:07:27 +0900

    Divide functions

---
+++ parser.js
@@ -0,0 +1,86 @@
+import Moment from "moment";
+
+const DELIMITER = " ";
+
+
+export default class Parser {
+
+    constructor(line) {
+        this.line = line;
+    }
+
+    _object(parts) {
+        let data = {};
+        data.BucketOwner = parts[0];
+        data.Bucket      = parts[1];
+        data.Datetime    = parts[2];
+        data.RemoteIP    = parts[3];
+        data.Requester   = parts[4];
+        data.RequestID   = parts[5];
+        data.Operation   = parts[6];
+        data.Key         = parts[7];
+        data.RequestURI  = parts[8];
+        data.HTTPStatus  = parts[9];
+        data.ErrorCode   = parts[10];
+        data.BytesSent   = parts[11];
+        data.ObjectSize  = parts[12];
+        data.TotalTime   = parts[13];
+        data.TurnAroundTime = parts[14];
+        data.Referrer    = parts[15];
+        data.UserAgent   = parts[16];
+        data.VersionID   = parts[17];
+        return data;
+    }
+
+    _format(data) {
+
+        data.date = Moment(data.date, "DD/MMM/YYYY:HH:mm:ss ZZ").unix();
+
+        return data;
+    }
+
+    parse() {
+        let parts = [];
+        let restString = this.line;
+        let closeCharacter = DELIMITER;
+
+        Analyze: while (true) {
+
+            // if restString doesn't include DELIMITER " ", it's a final token
+            if (!restString.includes(DELIMITER)) {
+                parts.push(restString);
+                break Analyze;
+            }
+
+            let closeAt = restString.indexOf(closeCharacter);
+            parts.push(restString.slice(closeCharacter.length - 1, closeAt));
+
+            restString = restString.slice(closeAt + closeCharacter.length);
+            if (restString[0] === "[") {
+                closeCharacter = "] ";
+            } else if (restString[0] === "\"") {
+                closeCharacter = "\" ";
+            } else {
+                closeCharacter = DELIMITER;
+            }
+        }
+
+        this.data = this._format(this._object(parts));
+
+        return this;
+    }
+
+    toJSON() {
+        return JSON.stringify(this.data, null, 4);
+    }
+}
+
+
+
+
+
+
+
+
+
+
