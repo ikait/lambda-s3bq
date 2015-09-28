82e23bb672ef7681d0c0740f92397c2eb9d75150
Taishi Ikai <t@ikai.tokyo>
2015-09-28 14:35:04 +0900

    Add gulpfile.babel.js and add modules to package.json

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import path from "path";
+import gulp from "gulp";
+import zip from "gulp-zip";
+import babel from "gulp-babel";
+import mocha from "gulp-mocha";
+import eslint from "gulp-eslint";
+import install from "gulp-install";
+import plumber from "gulp-plumber";
+import sourcemaps from "gulp-sourcemaps";
+
+
+gulp.task("lint", () =>
+    gulp.src("src/**/*.js")
+        .pipe(eslint({useEslintrc: true}))
+        .pipe(eslint.format("stylish"))
+        .pipe(eslint.failAfterError()));
+
+gulp.task("test", () =>
+    gulp.src(["src/**/*.js"])
+        .pipe(mocha({require: ["espower-babel/guess"]})));
+
+gulp.task("babel", () =>
+    gulp.src("src/**/*.js")
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("build")));
+
+gulp.task("install", () =>
+    gulp.src("./package.json")
+        .pipe(gulp.dest("build"))
+        .pipe(install({production: true})));
+
+gulp.task("zip", () =>
+    gulp.src("./build/**/*")
+        .pipe(zip("build.zip"))
+        .pipe(gulp.dest(".")));
+
+gulp.task("build", ["babel", "install"]);
